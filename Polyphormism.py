62c672cfa50e5f19f7abe30c90fbb53b8539819f
Promise Sibanda <brilliantpromise0@gmail.com>
2025-04-25 13:27:42 +0200

    Add files via upload

---
+++ Polyphormism.py
@@ -0,0 +1,10 @@
+class Animals:
+    def Dog_action(self):
+        print ("Dog is barking")
+
+    def Snake_action(self):
+        print ("Snake is hissing")
+
+Animal = Animals()
+Animal.Dog_action()
+Animal.Snake_action()
